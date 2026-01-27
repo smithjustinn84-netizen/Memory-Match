633cf65d2ae7f244e7eecf9563322e06c6ed519c
Justin Smith <smithjustinn84@gmail.com>
2026-01-26 20:40:14 -0500

    refactor: remove higher difficulty levels and update comments
    Removes the "Grandmaster" (14 pairs) and "Elephant Brain" (16 pairs) difficulty levels to simplify the game progression. The associated initial time configurations for these levels have also been removed from the game logic.
    
    This change also updates the in-game match comments to a poker theme, replacing generic feedback like "Halfway" and "Keep it up" with more thematic phrases such as "Pot Odds" and "Full House." The corresponding unit tests have been updated to reflect these new comments.

--- shared/core/src/commonMain/kotlin/io/github/smithjustinn/domain/models/DifficultyLevel.kt
+++ shared/core/src/commonMain/kotlin/io/github/smithjustinn/domain/models/DifficultyLevel.kt
@@ -2,8 +2,6 @@ package io.github.smithjustinn.domain.models
 
 import io.github.smithjustinn.resources.Res
 import io.github.smithjustinn.resources.difficulty_casual
-import io.github.smithjustinn.resources.difficulty_elephant
-import io.github.smithjustinn.resources.difficulty_grandmaster
 import io.github.smithjustinn.resources.difficulty_master
 import io.github.smithjustinn.resources.difficulty_shark
 import io.github.smithjustinn.resources.difficulty_toddler
@@ -23,8 +21,6 @@ data class DifficultyLevel(
                 DifficultyLevel(Res.string.difficulty_casual, 8),
                 DifficultyLevel(Res.string.difficulty_master, 10),
                 DifficultyLevel(Res.string.difficulty_shark, 12),
-                DifficultyLevel(Res.string.difficulty_grandmaster, 14),
-                DifficultyLevel(Res.string.difficulty_elephant, 16),
             )
     }
 }
